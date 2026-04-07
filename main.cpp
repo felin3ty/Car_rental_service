e9b83523a701e13b6d26252b42bf112b3c9089d3
fel <tsekrelis2007@gmail.com>
2026-04-07 12:54:22 +0300

    Enhance Vehicles class and add Customers/Reservation classes
    Refactor Vehicles class to include model_type and update constructor. Add Customers and Reservation classes with necessary attributes and constructors.

--- main.cpp
+++ main.cpp
@@ -5,20 +5,53 @@
 using namespace std;
 
 class Vehicles{ //δημιουργουμε αρχικα τις κλασεις που θα χρησιμοποιησουμε    
-protected:
+private:
     string license_plate;
+    string model_type;
     string category;
     string fuel_type;
     bool available;
     float daily_cost;
 
     public:
-        Vehicles(string plate , string categ , string fuel , float cost)  // αυτος ειναι ο constructor του Vehicles
-        : license_plate(plate) , category(categ) , fuel_type(fuel) ,available(true) ,  daily_cost(cost){}
+        Vehicles(string plate , string model , string categ , string fuel , float cost)  // αυτος ειναι ο constructor του Vehicles
+        : license_plate(plate) ,model_type(model) ,  category(categ) , fuel_type(fuel) ,available(true) ,  daily_cost(cost){}
     
     virtual ~Vehicles() = default; //αυτος ειναι ο destructor
 
     string getlicense_plate() const{ return license_plate;}
     float getdaily_cost() const{return daily_cost;}
+
+    virtual void printDetails() const{ //εδω κανουμε το προγραμμα να εμφανιζει τις πληροφοριες του καθε αμαξιου
+        cout<<"Vehicle: " << model_type << " ["  << license_plate << "] - " << daily_cost << "$/day\n"; 
+    }  
 };
 
+
+class Customers{//κλαση με τα χαρακτηριστικα για τους πελατες
+    private:
+        string customer_name;
+        int customer_id;
+
+    public:
+        Customers(int id , string name)
+        : customer_id(id) , customer_name(name){}
+
+    string getName() const{return customer_name;}
+    int getID() const{return customer_id;}
+};
+
+
+class Reservation{//κλαση για τις κρατησεις
+    private:
+        int Reservation_number;
+        Customers* Customer;    //δεικτες για το ποιος πελατης θελει ποιο οχημα
+        Vehicles* Vehicle;
+        time_t Start_Date; //ημερομηνιες για την κρατηση
+        time_t End_Date;
+        bool Reservation_Status;
+
+        public:
+            Reservation(int number , Customers* c , Vehicles* v , time_t start , time_t end , bool status)
+            :Reservation_number(number) , Customer(c) , Vehicle(v) , Start_Date(start) , End_Date(end) ,  Reservation_Status(status){}
+};
