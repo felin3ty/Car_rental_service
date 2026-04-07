ab78861b226b48d66a37362f9e40f24595468033
fel <tsekrelis2007@gmail.com>
2026-04-07 20:15:32 +0300

    Add vehicle rental system classes
    Implement vehicle, customer, reservation, and rental contract classes for a vehicle rental system.

---
+++ v0.1.1.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <string>
+#include<vector>
+
+using namespace std;
+
+class Vehicles{ //δημιουργουμε αρχικα τις κλασεις που θα χρησιμοποιησουμε    
+private:
+    string license_plate;
+    string model_type;
+    string category;
+    string fuel_type;
+    bool available;
+    float daily_cost;
+
+    public:
+        Vehicles(string plate , string model , string categ , string fuel , float cost)  // αυτος ειναι ο constructor του Vehicles
+        : license_plate(plate) ,model_type(model) ,  category(categ) , fuel_type(fuel) ,available(true) ,  daily_cost(cost){}
+    
+    virtual ~Vehicles() = default; //αυτος ειναι ο destructor
+
+    string getlicense_plate() const{ return license_plate;}
+    float getdaily_cost() const{return daily_cost;}
+
+    virtual void printDetails() const{ //εδω κανουμε το προγραμμα να εμφανιζει τις πληροφοριες του καθε αμαξιου
+        cout<<"Vehicle: " << model_type << " ["  << license_plate << "] - " << daily_cost << "$/day\n"; 
+    }  
+};
+
+
+class Customers{//κλαση με τα χαρακτηριστικα για τους πελατες
+    private:
+        string customer_name;
+        int customer_id;
+
+    public:
+        Customers(int id , string name)
+        : customer_id(id) , customer_name(name){}
+
+    string getName() const{return customer_name;}
+    int getID() const{return customer_id;}
+};
+
+
+class Reservation{//κλαση για τις κρατησεις
+    protected:
+        int Reservation_number;
+        Customers* Customer;    //δεικτες για το ποιος πελατης θελει ποιο οχημα
+        Vehicles* Vehicle;
+        time_t Start_Date; //ημερομηνιες για την κρατηση
+        time_t End_Date;
+        bool Reservation_Status = true;
+
+        public:
+            Vehicles* getVehicle() const { return Vehicle; }
+            Reservation(int number , Customers* c , Vehicles* v , time_t start , time_t end)
+            :Reservation_number(number) , Customer(c) , Vehicle(v) , Start_Date(start) , End_Date(end){}
+            
+            bool double_booking(time_t new_start , time_t new_end) const{ //Εδω ελεγχει αμα εχει γινει διπλοκρατηση
+                if(Reservation_Status == false){ //αν το στατους της κρατησης ειναι φαλσ τοτε δεν δημιουργειται διπλοκρατηση
+                    return false;
+                }
+                else{
+                    return(new_start<End_Date) && (new_end>Start_Date); //εδω ελεγχει τις ημερομηνιες και βλεπει αμα θα δημιουργηθει διπλοκρατηση και αμα γινει επιστεφει Α ή Ψ 
+                }
+            }
+};
+
+class RentalContract: public Reservation{ //κληρονομικοτητα απο την πανω καλση 
+    private:
+        time_t return_date;
+        bool has_insurance;
+    
+    public:
+        RentalContract(int number , Customers* c , Vehicles* v , time_t start , time_t end , time_t return_d , bool insurance )// χρησιμοποοιυμε πρωτα τον constructor του Reservation
+        : Reservation(number , c , v ,  start , end) , return_date(return_d) , has_insurance(insurance){}
+
+        float Total_Cost() const{ //υπολογισμος του τελικου κοστους
+            int booked_days = End_Date - Start_Date;
+            if(End_Date - Start_Date == 0){
+                booked_days = 1;
+            }
+        }
+};
